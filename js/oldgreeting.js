30ea7fcac9940b49a2f0cb1e5fcab0f6349546d0
Dyantho <24285@ma-web.nl>
2023-10-16 10:47:42 +0200

    oldgreeting.js

---
+++ js/oldgreeting.js
@@ -0,0 +1,10 @@
+ function greet()
+{
+    let greeting="Greetings AI, i welcome you to this world!";
+    console.log(greeting);
+}
+
+module.exports = {greet};
+["Toward those short trees",
+"We saw a hawk descending",
+"On a day in spring."]
